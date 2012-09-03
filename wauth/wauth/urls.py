832f6f3e7603d6dbd056d86cde3f75cdfec0ee8a
Michael K <mking@arikel.net>
2012-09-03 02:07:43 +0000

    Updated urls.

--- wauth/wauth/urls.py
+++ wauth/wauth/urls.py
@@ -1,20 +1,17 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.simple import redirect_to
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'wauth.views.home', name='home'),
-    # url(r'^wauth/', include('wauth.foo.urls')),
-
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-    (r'^user/', include('userena.urls')),
-    (r'^messages/', include('userena.contrib.umessages.urls')),
-
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^user/', include('userena.urls')),
+    url(r'^messages/', include('userena.contrib.umessages.urls')),
+    url(r'^auth/', include('apicore.urls')),
+    url(r'^/$', redirect_to, {'url': '/user/signin/'}),
 )
