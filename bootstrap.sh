838fb182268288a9b7190633c5fe96ba2b826a1d
Chris Baker <dosman711@gmail.com>
2013-06-24 21:25:32 -0400

    Adding Vagrant files, updating README

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y build-essential python-dev python-pip git python-virtualenv python-psycopg2 postgresql postgresql-server-dev-all sqlite3 screen libjpeg8 libjpeg62-dev libfreetype6 libfreetype6-dev
+
+ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib
+ln -s /usr/lib/x86_64-linux-gnu/libfreetype.so /usr/lib
+ln -s /usr/lib/x86_64-linux-gnu/libz.so /usr/lib
